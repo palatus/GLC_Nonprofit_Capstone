0be4c3dc18f54c4371510c78055dd415ffce4a6d
palatus <pogolad246@gmail.com>
2021-02-26 04:52:38 -0500

    Events Page & Views

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -9,15 +9,51 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+		<link href = {{ asset("bootstrap/css/bootstrap.css") }} rel="stylesheet" />
+		<link rel="stylesheet" href="{{ asset('bootstrap/css/bootstrap-theme.min.css') }}">
+		<link href="/css/app.css" rel="stylesheet">
+		
         <!-- Styles -->
         <style>
             
         </style>
     </head>
     <body>
-		
-		<div> Test </div>
+        		
+        <div class="container-fluid">
+        	<div class="row">
+        		<div class="col-md-12">
+        			<h3 class="text-center">
+        				Upcoming Events
+        			</h3>
+        			<nav>
+        				<ul class="pagination">
+        					<li class="page-item">
+        						<a class="page-link" href="#">Previous</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">1</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">2</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">3</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">4</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">5</a>
+        					</li>
+        					<li class="page-item">
+        						<a class="page-link" href="#">Next</a>
+        					</li>
+        				</ul>
+        			</nav>
+        		</div>
+        	</div>
+        </div>
 		
     </body>
 </html>
